13e5592f242409a55efaf9d40d54e51773a47241
Luke Whiteley <luke@whiteley.io>
2022-12-06 20:55:12 +0000

    Adds less lazy day 6 solution

--- twenty22/lib/006.ex
+++ twenty22/lib/006.ex
@@ -1,12 +1,11 @@
 defmodule Day006 do
 
   def find_marker(signal, chunk_size \\ 4) do
-    marker = String.graphemes(signal)
+    {_ , marker} = String.graphemes(signal)
     |> Enum.chunk_every(chunk_size, 1, :discard)
-    |> Enum.filter(fn x -> length(Enum.uniq(x)) == chunk_size end)
-    |> hd() |> Enum.join()
+    |> Enum.with_index() |> Enum.filter(fn {x, _} -> length(Enum.uniq(x)) == chunk_size end)
+    |> hd()
 
-    {start, len} = :binary.match(signal, marker) # I know how lazy this is
-    start + len
+    marker + chunk_size
   end
 end
