7b5ee3091b377815208f421f07c157b8547cd66c
Luke Whiteley <luke@whiteley.io>
2024-12-03 09:42:07 +0000

    Removes 2024 puzzle input

---
+++ twenty24/src/main/kotlin/Day3.kt
@@ -0,0 +1,14 @@
+
+object Day3 : Day<Int>() {
+    override val number = "003"
+
+    override fun partOne(input: List<String>): Int {
+
+        return 0
+    }
+
+    override fun partTwo(input: List<String>): Int {
+
+        return 0
+    }
+}
