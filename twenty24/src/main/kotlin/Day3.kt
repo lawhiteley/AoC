dad210f614608a2d97e527d85d7351937bdb6b1d
Luke Whiteley <luke@whiteley.io>
2024-12-03 11:03:45 +0000

    Adds 2024 day 3

--- twenty24/src/main/kotlin/Day3.kt
+++ twenty24/src/main/kotlin/Day3.kt
@@ -2,13 +2,30 @@
 object Day3 : Day<Int>() {
     override val number = "003"
 
-    override fun partOne(input: List<String>): Int {
+    private val multiplyFunctionPattern = "mul\\((\\d+),(\\d+)\\)".toRegex()
+    private val patternWithStartStop = "$multiplyFunctionPattern|(do\\(\\))|(don't\\(\\))".toRegex()
 
-        return 0
-    }
+    override fun partOne(input: List<String>): Int =
+        multiplyFunctionPattern.findAll(input.joinToString { it }).map {
+            executeMultiplication(it.value)
+        }.sum()
 
-    override fun partTwo(input: List<String>): Int {
+    override fun partTwo(input: List<String>) = patternWithStartStop.findAll(input.joinToString { it })
+        .fold(Pair(false, mutableListOf<String>())) { (stopped, validCalls), item ->
+            with(item.value) {
+                when {
+                    this == "don't()" -> true to validCalls
+                    this == "do()" -> false to validCalls
+                    !stopped -> false to validCalls.apply { add(this@with) }
+                    else -> stopped to validCalls
+                }
+            }
+        }.second.sumOf { executeMultiplication(it) }
 
-        return 0
+    private fun executeMultiplication(functionCall: String): Int {
+        val match = multiplyFunctionPattern.find(functionCall)
+        val (_, a, b) = match?.groupValues!!
+
+        return a.toInt() * b.toInt()
     }
 }
--- twenty24/src/test/kotlin/Solutions.kt
+++ twenty24/src/test/kotlin/Solutions.kt
@@ -7,29 +7,30 @@ class Solutions : FunSpec({
     context("2024 Advent of Code") {
         withData(
             listOf(
-//                Day1 to Answers(11, 1938424, 31, 22014209),
-//                Day2 to Answers(2, 341, 4, 404),
-                Day3 to Answers(2, 341, 4, 404),
+                Day1 to Answers(e1 = 11, e2 = 31, p1 = 1938424, p2 = 22014209),
+                Day2 to Answers(e1 = 2, e2 = 4, p1 = 341, p2 = 404),
+                Day3 to Answers(e1 = 161, e2 = 48, p1 = 183380722, p2 = 82733683),
             ),
         ) { (day, answers) ->
             this@context.context("Solutions for day ${day.number}") {
-                val example = readResource("${day.number}-input.txt")
-                val input = readResource("${day.number}-example.txt")
+                val exampleOne = readResource("${day.number}-example-1.txt")
+                val exampleTwo = readResource("${day.number}-example-2.txt")
+                val input = readResource("${day.number}-input.txt")
 
                 test("example 1") {
-                    day.partOne(example) shouldBe answers.firstExample
+                    day.partOne(exampleOne) shouldBe answers.e1
                 }
 
                 test("puzzle 1") {
-                    day.partOne(input) shouldBe answers.secondExample
+                    day.partOne(input) shouldBe answers.p1
                 }
 
                 test("example 2") {
-                    day.partTwo(example) shouldBe answers.firstPuzzle
+                    day.partTwo(exampleTwo) shouldBe answers.e2
                 }
 
                 test("puzzle 2") {
-                    day.partTwo(input) shouldBe answers.secondPuzzle
+                    day.partTwo(input) shouldBe answers.p2
                 }
             }
         }
@@ -37,8 +38,8 @@ class Solutions : FunSpec({
 })
 
 data class Answers(
-    val firstExample: Int,
-    val secondExample: Int,
-    val firstPuzzle: Int,
-    val secondPuzzle: Int,
+    val e1: Any,
+    val e2: Any,
+    val p1: Any,
+    val p2: Any,
 )
