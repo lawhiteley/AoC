d5fc47e2ec15c781bc65e7299782ce86ff498a3a
Luke Whiteley <luke@whiteley.io>
2025-12-03 17:49:11 +0000

    Adds 2025 day 3 part 2

--- twenty25/lib/003.ex
+++ twenty25/lib/003.ex
@@ -1,16 +1,46 @@
 defmodule Day003 do
-  def maximum_joltage(banks) do
+  def maximum_joltage_from_two(banks) do
     banks
     |> Enum.map(&find_best_two(String.split(&1, "", trim: true)))
     |> Enum.sum()
   end
 
-  defp find_best_two(list) do
-    indexed = Enum.with_index(list)
+  defp find_best_two(cells) do
+    indexed = Enum.with_index(cells)
 
     for {a, i} <- indexed, {b, j} <- indexed, i < j do
       String.to_integer(a <> b)
     end
     |> Enum.max()
+  end
+
+  def maximum_joltage_from_twelve(banks) do
+    banks
+    |> Enum.map(&String.split(&1, "", trim: true))
+    |> Enum.map(fn bank ->
+      find_best_twelve(bank |> Enum.map(&String.to_integer/1))
+    end)
+    |> Enum.sum()
+  end
+
+  def find_best_twelve(cells) do
+    cells
+    |> Enum.with_index()
+    |> Enum.chunk_every(length(cells) - 11, 1, :discard)
+    |> Enum.reduce({[], -1}, fn window, {acc, used} ->
+      {highest_val, highest_idx} = find_next_highest(window, used)
+
+      {acc ++ [highest_val], highest_idx}
+    end)
+    |> elem(0)
+    |> Enum.join()
+    |> String.to_integer()
+  end
+
+  defp find_next_highest(window, used) do
+    case Enum.reject(window, fn {_, i} -> used >= i end) do
+      [] -> {nil, used}
+      list -> Enum.max_by(list, fn {v, _} -> v end)
+    end
   end
 end
--- twenty25/test/003_test.exs
+++ twenty25/test/003_test.exs
@@ -5,14 +5,14 @@ defmodule Day003Test do
   @example TestHelper.file_to_strings("./test/data/003-example.txt")
 
   test "day 3 example" do
-    assert Day003.maximum_joltage(@example) == 357
+    assert Day003.maximum_joltage_from_two(@example) == 357
   end
 
   test "day 3 puzzle 1" do
-    assert Day003.maximum_joltage(@data) == 24_747_430_309
+    assert Day003.maximum_joltage_from_two(@data) == 17_301
   end
 
-  # test "day 3 puzzle 2" do
-  #   assert Day003.id_repeats_at_least_twice(@data) == 30_962_646_823
-  # end
+  test "day 3 puzzle 2" do
+    assert Day003.maximum_joltage_from_twelve(@data) == 172_162_399_742_349
+  end
 end
