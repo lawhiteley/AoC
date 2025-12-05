2bdeb45b240de3e341ce572ad3a08fec1abffde9
Luke Whiteley <luke@whiteley.io>
2025-12-05 15:04:48 +0000

    Adds 2025 day 5 part 2

--- twenty25/lib/005.ex
+++ twenty25/lib/005.ex
@@ -1,17 +1,47 @@
 defmodule Day005 do
-
   require Logger
 
-  def find_spoiled(ingredients) do
+  def count_spoiled(ingredients) do
     [ranges, ingredients] =
+      ingredients
+      |> String.split("\n\n")
+      |> Enum.map(&String.split(&1, "\n"))
+
+    ranges = Enum.map(ranges, &string_to_range/1)
+
+    Enum.count(ingredients, fn ingredient ->
+      Enum.any?(ranges, &(String.to_integer(ingredient) in &1))
+    end)
+  end
+
+  def count_fresh(ingredients) do
+    [ranges, _] =
       String.split(ingredients, "\n\n") |> Enum.map(&String.replace(&1, "\n", ","))
 
-      # don't need two String.split calls
     ranges = String.split(ranges, ",") |> Enum.map(&string_to_range/1)
 
-    String.split(ingredients, ",") |> Enum.filter(fn ingredient ->
-      Enum.any?(ranges, &(String.to_integer(ingredient) in &1))
-    end) |> length()
+    remove_overlaps(ranges) |> Enum.map(&Range.size(&1)) |> Enum.sum()
+  end
+
+  defp remove_overlaps(ranges) do
+    ranges
+    |> Enum.sort_by(fn start.._//_ -> start end)
+    |> Enum.reduce([], fn current, acc ->
+      case acc do
+        [] ->
+          [current]
+
+        [last | rest] ->
+          beginning_of_first..end_of_first//_ = last
+          _..end_of_second//_ = current
+
+          cond do
+            Range.disjoint?(last, current) -> [current | acc]
+            end_of_second <= end_of_first -> acc
+            true -> [beginning_of_first..max(end_of_first, end_of_second) | rest]
+          end
+      end
+    end)
   end
 
   def string_to_range(string) do
--- twenty25/test/005_test.exs
+++ twenty25/test/005_test.exs
@@ -5,14 +5,14 @@ defmodule Day005Test do
   @example TestHelper.file_to_string("./test/data/005-example.txt")
 
   test "day 5 example" do
-    assert Day005.find_spoiled(@example) == 3
+    assert Day005.count_spoiled(@example) == 3
   end
 
   test "day 5 puzzle 1" do
-    assert Day005.find_spoiled(@data) == 885
+    assert Day005.count_spoiled(@data) == 885
   end
 
-  # test "day 5 puzzle 2" do
-  #   assert Day005.count_possible_removals(@data, 140) == 8310
-  # end
+  test "day 5 puzzle 2" do
+    assert Day005.count_fresh(@data) == 348_115_621_205_535
+  end
 end
