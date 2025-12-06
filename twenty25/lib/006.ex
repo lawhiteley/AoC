923acae0c36933d35f760afd9e70e681d127836c
Luke Whiteley <luke@whiteley.io>
2025-12-06 12:32:23 +0000

    Adds 2025 day 6 part 2

--- twenty25/lib/006.ex
+++ twenty25/lib/006.ex
@@ -9,6 +9,20 @@ defmodule Day006 do
     |> Enum.sum()
   end
 
+  def calculate_right_to_left(worksheet) do
+    [operands | values] = Enum.reverse(worksheet)
+
+    values |> Enum.map(&String.graphemes/1)
+    |> Enum.zip_with(&Function.identity/1)
+    |> Enum.chunk_by(&column?/1) |> Enum.reject(&(column?(hd(&1))))
+    |> Enum.map(fn column ->
+      Enum.map(column, &(Enum.reverse(&1) |> Enum.join() |> String.trim()))
+    end)
+    |> Enum.zip(String.split(operands, " ", trim: true))
+    |> Enum.map(&calculate/1)
+    |> Enum.sum()
+  end
+
   defp calculate({values, operand}) do
     values = Enum.map(values, &String.to_integer/1)
 
@@ -17,4 +31,6 @@ defmodule Day006 do
       "*" -> Enum.product(values)
     end
   end
+
+  def column?(list), do: Enum.all?(list, &(&1 == " "))
 end
--- twenty25/test/006_test.exs
+++ twenty25/test/006_test.exs
@@ -1,18 +1,18 @@
 defmodule Day006Test do
   use ExUnit.Case, async: true
 
-  @data TestHelper.file_to_strings("./test/data/006.txt")
-  @example TestHelper.file_to_strings("./test/data/006-example.txt")
+  @data TestHelper.file_to_strings("./test/data/006.txt", :full)
+  @example TestHelper.file_to_strings("./test/data/006-example.txt", :full)
 
   test "day 6 example" do
     assert Day006.calculate_vertical(@example) == 4_277_556
   end
 
   test "day 6 puzzle 1" do
-    assert Day006.calculate_vertical(@data) == 885
+    assert Day006.calculate_vertical(@data) == 4_580_995_422_905
   end
 
-  # test "day 6 puzzle 2" do
-  #   assert Day006.count_fresh(@data) == 348_115_621_205_535
-  # end
+  test "day 6 puzzle 2" do
+    assert Day006.calculate_right_to_left(@data) == 10_875_057_285_868
+  end
 end
