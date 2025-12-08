14f52294f7d7a172656d32cb30301a52327c22c0
Luke Whiteley <luke@whiteley.io>
2025-12-08 20:23:57 +0000

    Refactors 2025 day 8

--- twenty25/lib/008.ex
+++ twenty25/lib/008.ex
@@ -38,11 +38,7 @@ defmodule Day008 do
   end
 
   defp euclidian_distance(p1, p2) do
-    :math.sqrt(
-      :math.pow(p2.x - p1.x, 2) +
-        :math.pow(p2.y - p1.y, 2) +
-        :math.pow(p2.z - p1.z, 2)
-    )
+    :math.sqrt(:math.pow(p2.x - p1.x, 2) + :math.pow(p2.y - p1.y, 2) + :math.pow(p2.z - p1.z, 2))
   end
 
   # From https://rosettacode.org/wiki/Combinations#Elixir
